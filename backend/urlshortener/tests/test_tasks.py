bcbaf5e68665d8bbfe0916a1c4db813213d7ed29
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-09 09:26:29 +0200

    test: add tasks test for tracking clicks

--- backend/urlshortener/tests/test_tasks.py
+++ backend/urlshortener/tests/test_tasks.py
@@ -1,1 +1,16 @@
 import pytest
+from ..tasks import track_click
+from ..models import ShortenedURL
+
+@pytest.mark.django_db
+def test_track_click():
+    """Test that the track_click function updates the click count."""
+    data = {
+        "original_url": "https://www.example.com",
+        "short_code": "abc123abc1"
+    }
+    url = ShortenedURL.objects.create(**data)
+    assert url.clicks == 0
+    track_click(url)
+    url.refresh_from_db()
+    assert url.clicks == 1
